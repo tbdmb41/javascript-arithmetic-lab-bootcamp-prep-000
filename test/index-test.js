af1b90679026733672c6baa5bc6597f6f1be2ac3
Thomas Bowen <tbowen1@outlook.com>
2017-10-24 17:34:12 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -5,19 +5,19 @@ beforeEach(function() {
   b = Math.floor(Math.random() * 1000)
 })
 
-it('add(a, b) adds two numbers and returns the result', function() {
+it('add(a, b) adds two numbers and returns the result', add(a+b) {
   expect(add(a, b)).toEqual(a + b)
 })
 
-it('subtract(a, b) subtracts b from a and returns the result', function() {
+it('subtract(a, b) subtracts b from a and returns the result', subtract(a-b) {
   expect(subtract(a, b)).toEqual(a - b)
 })
 
-it('multiply(a, b) multiplies two numbers and returns the result', function() {
+it('multiply(a, b) multiplies two numbers and returns the result', multiply(a*b) {
   expect(multiply(a, b)).toEqual(a * b)
 })
 
-it('divide(a, b) divides a by b and returns the result', function() {
+it('divide(a, b) divides a by b and returns the result', divide(a/b) {
   expect(divide(a, b)).toEqual(a / b)
 })
 
